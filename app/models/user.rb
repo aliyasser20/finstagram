6a12230281704c5ded1455e6556d0e2195992539
aliyasser20 <alisayed@Alis-MacBook-Pro.local>
2019-12-04 20:09:55 -0500

    completed w4 d2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
   has_many :comment
   has_many :like
 
+  validates :email, :username, uniqueness: true
+  validates :email, :username, :avatar_url, :password, presence: true
 end

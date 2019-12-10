412f9b3d9e00509e8918d220a0ba94700e193372
aliyasser20 <alisayed@Alis-MacBook-Pro.local>
2019-12-09 20:45:42 -0500

    week 5 day 1

--- app/actions.rb
+++ app/actions.rb
@@ -4,11 +4,20 @@
 # 2. open chrome ---- http://127.0.01:3000
 ###############################################
   
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+end
+
+## Home Page ##
 get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
 end
+###########################
 
+## Signup Page ##
 get '/signup' do
   @user = User.new
   erb(:signup)
@@ -24,7 +33,39 @@ post '/signup' do
 
   if @user.save
     "User #{username} saved!"
+    redirect to('/login')
   else
     erb(:signup)
   end
 end
+###########################
+
+## Sign in Page ##
+get '/login' do
+  erb(:login)
+end
+
+post '/login' do
+  username = params[:username]
+  password = params[:password]
+
+  @user = User.find_by(username: username)  
+
+  if @user && @user.password == password
+    session[:user_id] = @user.id
+    "Success! User with id #{session[:user_id]} is logged in!"
+    redirect to('/')
+  else
+    @error_message = "Login failed."
+    erb(:login)
+  end
+end
+###########################
+
+## Sign out ##
+get '/logout' do
+  session[:user_id] = nil
+  "Logout successful!"
+  redirect to('/')
+end
+###########################
